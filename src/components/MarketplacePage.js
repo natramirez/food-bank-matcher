577e1e87178a437bf6202d9e67f7cbdf31f65ae8
Natalia Ramirez <natalia@Natalias-MacBook-Pro.local>
2018-08-22 09:58:48 -0700

    Marketplace page now loads data upon page loading

--- src/components/MarketplacePage.js
+++ src/components/MarketplacePage.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import WrappedSearchBar from './WrappedSearchBar';
-// import axios from 'axios';
 
 class MarketplacePage extends Component {
     constructor(props) {
         super(props)
-        this.state = this.getInitialState();
+        this._isMounted = false;
+        this.state = {};
         this.displayResults = this.displayResults.bind(this);
         this.createResultBoxes = this.createResultBoxes.bind(this);
 
     }
-    getInitialState() {
-        return {
-            results:null
-        }
+    componentDidMount() {
+        this._isMounted = true;
+        this.displayResults("", "Any");
+    }
+    componentWillUnmount() {
+        this._isMounted = false;
     }
     createResult = (res,index) => {
         var reserveBtn = <button label="Reserve" value="Reserve"/>;
@@ -35,45 +37,21 @@ class MarketplacePage extends Component {
         var params;
         if (item && category) {
             params = `itemName=${item}&categories=${category}`
-            // url = `http://localhost:8080/api/searchByItemAndCategory?itemName=${item}&categories=${category}`;
         } else {
             params = item ? `itemName=${item}` : `categories=${category}`;
-            // url = item ? 'http://localhost:8080/api/searchByItem?itemName='+item : 'http://localhost:8080/api/searchByCategory?categories='+category; 
         }
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url + params);
         xhr.onload = function() {
+            if (!this._isMounted) {return;}
             var data = JSON.parse(xhr.response);
-            console.log("data: ", data);
-            console.log("xhr status: ", xhr.status);
-            console.log("data name: ", data.name);
-
-            if ((xhr.status >= 200 && xhr.status < 400) && (!data.name)) {
-                console.log("here")
-                this.setState({results: data});
+            if ((xhr.status >= 200 && xhr.status < 400) && (!data.name) && this._isMounted) {
+                this.setState({results: data, errorMsg:''});
             } else {
-                console.log("xhr status: ", xhr.status);
                 this.setState({errorMsg: "An error occurred. Please try again later."});
             }
         }.bind(this);
         xhr.send();
-        // var that = this;
-        // axios.get(url).then(res => {
-        //     console.log('res: ' + JSON.stringify(res));
-
-        //     console.log('res.data: ' + JSON.stringify(res.data));
-        //     // this.questions = res.data;
-        //     // if (res.data.name && res.data.name === "MongoError") {
-        //     //     // this.handleError();
-        //     // }
-        //     // else {
-        //     console.log("Res: " + res.body)
-        //     // that.resultsReceived = true;
-        //     that.results = res;
-        //     that.setState({results:res.body});
-        //         // this.setState({message: null});                
-        //     // }
-        // });
     }
     render() {
     return (
@@ -82,9 +60,6 @@ class MarketplacePage extends Component {
             <WrappedSearchBar displayResults={this.displayResults}/>
             {this.state.results ? (<div>{this.createResultBoxes(this.state.results)}</div>) : ""}
             {this.state.errorMsg ? (<div>{this.state.errorMsg}</div>) : ""}
-            {/* <div>
-                {this.createResultBoxes()}
-            </div> */}
         </div>
     );
   }
